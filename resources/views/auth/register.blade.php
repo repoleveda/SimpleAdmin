69dbcf54f608fd4f1a8fba069794934c5c5129f2
xianbian <lqsxianbian@yandex.com>
2016-10-11 18:38:00 +0800

    2016年10月11日18:37:57

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,10 +39,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">trans("lang.password"</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input id="password" type="password" class="form-control" name=password required>
 
                                 @if ($errors->has('password'))
                                     <span class="help-block">
